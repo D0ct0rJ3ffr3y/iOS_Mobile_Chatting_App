de14cd56a33e3800ed7f50283b22802af649921f
Enrique Aylagas <aylagase@gmail.com>
2020-10-06 16:26:46 -0400

    finished part 14 (photo messages)

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import FirebaseDatabase
+import MessageKit
 
 final class DatabaseManager{
     
@@ -422,6 +423,26 @@ extension DatabaseManager{
                       let date = ChatViewController.dateFormatter.date(from: dateString)else {
                     return nil
                 }
+                var kind: MessageKind?
+                
+                if type == "photo"{
+                    //photo
+                    guard let imageUrl = URL(string: content),
+                          let placeHolder = UIImage(systemName: "plus") else{
+                        return nil
+                    }
+                    let media = Media(url: imageUrl,
+                                      image: nil,
+                                      placeholderImage: placeHolder,
+                                      size: CGSize(width: 300, height: 300))
+                    kind = .photo(media)
+                }
+                else{
+                    kind = .text(content)
+                }
+                guard let finalKind = kind else{
+                    return nil
+                }
                 
                 let sender = Sender(photoURL: "",
                                     senderId: senderEmail,
@@ -430,7 +451,7 @@ extension DatabaseManager{
                 return Message(sender: sender,
                                messageId: messageID,
                                sentDate: date,
-                               kind: .text(content))
+                               kind: finalKind)
             })
             
             completion(.success(messages))
@@ -468,7 +489,10 @@ extension DatabaseManager{
                 message = messageText
             case .attributedText(_):
                 break
-            case .photo(_):
+            case .photo(let mediaItem):
+                if let targetUrlString = mediaItem.url?.absoluteString{
+                message = targetUrlString
+                }
                 break
             case .video(_):
                 break
